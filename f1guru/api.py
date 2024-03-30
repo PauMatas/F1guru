84c885c0a17d94ac93800cf6f292fa0fd8045ede
Pau Matas <61761147+PauMatas@users.noreply.github.com>
2024-03-30 16:47:45 +0100

    F1guru chain API implementation (#2)
    * add: api implemented
    
    * add: api mockup implemented to avoid ChatGPT billing while testing
    
    * fix: linting errors

---
+++ f1guru/api.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+from f1guru import F1GuruChain
+
+app = FastAPI()
+
+
+@app.get("/answer", methods=["GET"])
+def answer(q: str):
+    if not q:
+        raise HTTPException(status_code=400, detail="Missing question")
+
+    try:
+        return {"answer": F1GuruChain().answer_question(q)}
+    except Exception as e:  # TODO: Catch specific exception for problems with the chain
+        raise HTTPException(
+            status_code=404, detail="System is not capable to answer this question."
+        ) from e
+
+
+@app.get("/answer/mockup", methods=["GET"])
+def answer_mockup(q: str):
+    if not q:
+        raise HTTPException(status_code=400, detail="Missing question")
+
+    return {
+        "answer": "Fernando is faster than you. Can you confirm you understood this message?"
+    }
