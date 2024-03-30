8e0d1cc8032c5ca52c9f5346ab6d775eb73d9372
Pau Matas <pmatasalbiol@gmail.com>
2024-03-30 11:51:38 +0100

    add: api implemented

---
+++ f1guru/api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, HTTPException
+from f1guru import F1GuruChain
+
+app = FastAPI()
+
+
+@app.get("/answer", methods=["GET"])
+def answer(q: str):
+    if not q:
+        raise HTTPException(status_code=400, detail="Missing question")
+
+    try:
+        return {"answer": F1GuruChain().answer_question(q)}
+    except Exception as e:  # TODO: Catch specific exception for problems with the chain
+        raise HTTPException(
+            status_code=404, detail="System is not capable to answer this question."
+        )
