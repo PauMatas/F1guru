b3da27581e6160682fad7162bdf146d99a15b5a6
Pau Matas <pmatasalbiol@gmail.com>
2024-03-31 18:00:39 +0200

    style: format

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,26 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-	const [response, setResponse] = useState(undefined);
-	const [isLoading, setIsLoading] = useState(false);
+  const [response, setResponse] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
 
-	useEffect(() => {
-		const fetchResponse = async () => {
-			setIsLoading(true);
-			const response = await axios.get("/api/answer/mockup", {
-				params: { q: "What is Next.js?" },
-			});
-			setResponse(response.data.answer);
-			setIsLoading(false);
-		};
-		fetchResponse();
-	}, []);
+  useEffect(() => {
+    const fetchResponse = async () => {
+      setIsLoading(true);
+      const response = await axios.get("/api/answer/mockup", {
+        params: { q: "What is Next.js?" },
+      });
+      setResponse(response.data.answer);
+      setIsLoading(false);
+    };
+    fetchResponse();
+  }, []);
 
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="relative flex place-items-center">{isLoading ? <Skeleton /> : <p>{response}</p>}</div>
-		</main>
-	);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative flex place-items-center">
+        {isLoading ? <Skeleton /> : <p>{response}</p>}
+      </div>
+    </main>
+  );
 }
--- components/providers.tsx
+++ components/providers.tsx
@@ -4,20 +4,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-	const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-	return (
-		<QueryClientProvider client={queryClient}>
-			{/* @ts-ignore */}
-			<ThemeProvider
-				attribute="class"
-				defaultTheme="system"
-				enableSystem
-				disableTransitionOnChange>
-				{children}
-			</ThemeProvider>
-		</QueryClientProvider>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* @ts-ignore */}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
