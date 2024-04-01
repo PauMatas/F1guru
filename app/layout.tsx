e8987392e6a4a4c7a37a3fa911083a4906999868
Pau Matas <pmatasalbiol@gmail.com>
2024-04-01 10:32:54 +0200

    add: navbar updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head />
       <body className={cn(inter.className, "antialiased bg-background")}>
         <Providers>
-          <header
-            className={cn(
-              "top-0 sticky z-20 w-full py-3 bg-background flex flex-row flex-nowrap justify-between max-w-5xl mx-auto h-14 items-stretch animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out",
-              BODY_PADDING,
-            )}
-          >
-            <Navbar />
-          </header>
+          <Navbar />
           <main className={cn("min-h-screen flex items-stretch flex-col pb-28 max-w-5xl mx-auto", BODY_PADDING)}>
             {children}
           </main>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,29 @@
+import { cn } from "@/lib/utils";
 import { ModeToggle } from "./mode-toggle";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
+import { BODY_PADDING } from "@/app/config";
+import { Github } from "lucide-react";
 
 export function Navbar() {
   return (
-    <div className="fixed top-0 inset-x-0 h-auto bg-secondary border-b border-border z-[10] py-2">
-      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
-        <ModeToggle />
-      </div>
-    </div>
+    <header
+      className={cn(
+        "top-0 sticky z-20 w-full py-3 bg-background flex flex-row flex-nowrap justify-between max-w-5xl mx-auto h-14 items-stretch animate-in fade-in slide-in-from-top-4 duration-1000 ease-in-out",
+        BODY_PADDING,
+      )}
+    >
+      <Link
+        className={cn(
+          buttonVariants({ variant: "link", size: "lg" }),
+          "flex flex-row flex-nowrap items-center justify-center gap-x-1.5 pr-1.5 leading-none",
+        )}
+        href="/"
+      >
+        {/* Icons.icon */}
+        <span>🏎 F1 Guru</span>
+      </Link>
+      <ModeToggle />
+    </header>
   );
 }
